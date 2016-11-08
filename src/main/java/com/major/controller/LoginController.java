85c4e46bceb2fbce4d4b11e7019107adc62d2757
majormark <406596504@qq.com>
2016-11-08 23:56:43 +0800

    keyi

--- src/main/java/com/major/controller/LoginController.java
+++ src/main/java/com/major/controller/LoginController.java
@@ -9,15 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -28,19 +25,16 @@ public class LoginController {
 	@Autowired
 	RiskService riskService;
 
-	private HttpSession session = null;
-	
 	@RequestMapping(path = { "/", "/login" })
 	public String loginShow(){
 		return "login";
 	}
 
-	@RequestMapping(path = { "/loginUserHome" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(path = { "/loginUserHome" })
 	public String login(Model model, 
 			@RequestParam("username") String username,
 			@RequestParam("password") String password,
-			HttpServletRequest request,
-            HttpServletResponse response) {
+            HttpSession session) {
 		Map<String, Object> map = userService.login(username, password);
 		String msg = (String)map.get("msg");
 		if(msg.equals("该用户不存在!") || msg.equals("密码错误!")){
@@ -49,7 +43,6 @@ public class LoginController {
 		}
 		
 		model.addAttribute("user", (User)map.get("user"));
-		session = request.getSession(true);
 		session.setAttribute("user", (User)map.get("user"));
 		
 		List<Risk> riskList = riskService.getAllRisks();
@@ -58,4 +51,9 @@ public class LoginController {
 		return "user";
 	}
 
+	@RequestMapping(value = {"/logout"})
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "login";
+	}
 }
