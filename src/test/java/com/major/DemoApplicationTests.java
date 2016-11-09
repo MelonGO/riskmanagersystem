ad8e0e20f1d75d74c106753bd243d8dbd4c481d3
tatianguiqu <1013419927@qq.com>
2016-11-09 15:27:06 +0800

    数据层补全update等 注意页面有接口参数不一致

--- src/test/java/com/major/DemoApplicationTests.java
+++ src/test/java/com/major/DemoApplicationTests.java
@@ -2,16 +2,39 @@ package com.major;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.major.model.Project;
+import com.major.model.Risk;
+import com.major.model.RiskStateTrace;
+import com.major.service.ProjectService;
+import com.major.service.RiskService;
+import com.major.service.RiskStateTraceService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+	@Autowired
+	//ProjectService ps;
+//	RiskStateTraceService rs;
+	RiskService rs1;
 	@Test
 	public void contextLoads() {
-		System.out.println("test");
+	//ps.addProject("121");
+//		Project project =ps.getProject(1);
+//		project.setName("2b");
+//		ps.updateProject(project);
+//		rs.addRiskStateTrace(12, "sd", "dd");
+//		RiskStateTrace rst=rs.getRiskStateTrace(1);
+//		rst.setName("sdsd");
+//		rs.updateRiskStateTrace(rst);
+		rs1.addRisk(122,"类型", "nei", "ke", "ying", "chufa", 1, 2);
+//		Risk risk=rs1.getRisk(1);
+//		risk.setProjectId(12);
+//		risk.setContent("binan");
+//		rs1.updateRisk(risk);
 	}
 
 }
