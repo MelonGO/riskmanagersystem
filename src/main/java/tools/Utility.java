9c8399f9e55275efdc7e0617d64ae5009a2eb295
majormark <406596504@qq.com>
2016-11-11 16:32:43 +0800

    修改坏味道，增加数据库脚本

--- src/main/java/tools/Utility.java
+++ src/main/java/tools/Utility.java
@@ -5,7 +5,7 @@ public class Utility {
 	private Utility() {}
 	
 	public static boolean isEmptyString(String src){
-		return (src == null || src.trim().isEmpty());
+		return src == null || src.trim().isEmpty();
 	}
 	
 }
