75500fc4c3306453103b3f6de9e912eedcdc8a91
majormark <406596594@qq.com>
2016-11-15 10:09:04 +0800

    修改脚本语法错误 修改PlanController

--- src/main/java/com/major/controller/PlanController.java
+++ src/main/java/com/major/controller/PlanController.java
@@ -11,9 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.major.model.Plan;
 import com.major.model.Project;
-import com.major.model.Risk;
 import com.major.model.User;
+import com.major.service.PlanService;
 import com.major.service.ProjectService;
 
 import tools.RequestUtil;
@@ -39,15 +40,14 @@ public class PlanController {
 	
 	@RequestMapping(value = { "/getByPlanId" })
 	@ResponseBody
-	public Risk getByPlanId(@RequestParam("planId") Integer riskId){
-		//return riskService.getRisk(riskId);
+	public Plan getByPlanId(@RequestParam("planId") Integer planId){
+		return planService.getPlan(planId);
 	}
 	
 	@RequestMapping(value = { "/deletePlanById" })
 	@ResponseBody
-	public String deletePlan(@RequestParam("planId") String riskId){
-		//riskService.deleteRisk(Integer.parseInt(riskId));
-		
+	public String deletePlan(@RequestParam("planId") Integer planId){
+		planService.deletePlan(planId);
 		return "删除成功！";
 	}
 	
@@ -55,11 +55,18 @@ public class PlanController {
 	@ResponseBody
 	public String editPlan(HttpServletRequest request){
 		Integer projectId = RequestUtil.getPositiveInteger(request, "projectId", null);
-		if(projectId == null) {
-		
+		Integer planId  = RequestUtil.getPositiveInteger(request, "planId", null);
+		String name = RequestUtil.getString(request, "name", null);
+		if(planId == null) {
+			Map<String, Object> msgMap = planService.addPlan(projectId, name);
+			return (String) msgMap.get("msg");
 		} else {
-			
+			Plan plan = new Plan();
+			plan.setId(planId);
+			plan.setProjectId(projectId);
+			plan.setName(name);
+			Map<String, Object> msgMap = planService.updatePlan(plan);
+			return (String) msgMap.get("msg");
 		}
-		
 	}
 }
